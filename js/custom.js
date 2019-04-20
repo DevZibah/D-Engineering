1df713e9f12edeb710578236bf6dd964fdef1aa2
Yusuf Rafiu <yusuf_rafiu@yahoo.co.uk>
2019-04-20 08:12:31 +0100

    initial commit adding all files

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    var carouselContainer = $('.carousel');
+    var slideInterval = 2000;
+
+   /*<!--  function toggleCaption() {
+        $('.carousel-caption').hide();
+        var caption = carouselContainer.find('.active').find('.carousel-caption');
+        caption.delay(500).toggle("slide", {
+            direction: 'right'
+        });
+    }
+    
+    carouselContainer.carousel({
+        interval: slideInterval,
+        cycle: true,
+        pause: "hover"
+    }).on('slid.bs.carousel', function () {
+        toggleCaption();
+    }); -->*/
+
+});
