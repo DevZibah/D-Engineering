841ae55cd0b5961785518bc1bb51203482eb1328
kingsley <kinxly@gmail.com>
2021-03-16 20:50:20 +0100

    added animation to the hero carousel

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,17 @@
+
+// ==================================================
 $(function () {
+  var carouselContainer = $('.carousel')
+  var slideInterval = 5000
 
-    var carouselContainer = $('.carousel');
-    var slideInterval = 2000;
-
-   /*<!--  function toggleCaption() {
+  /*<!--  function toggleCaption() {
         $('.carousel-caption').hide();
         var caption = carouselContainer.find('.active').find('.carousel-caption');
         caption.delay(500).toggle("slide", {
             direction: 'right'
         });
     }
-    
+
     carouselContainer.carousel({
         interval: slideInterval,
         cycle: true,
@@ -18,5 +19,19 @@ $(function () {
     }).on('slid.bs.carousel', function () {
         toggleCaption();
     }); -->*/
+})
 
-});
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+
+// we implement the plugin for the scroll on reveal
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 2000,
+  reset: true,
+})
+
+// each html that has the class atribute of cc-scroll will therefore implement the scroll reveal method...
+sr.reveal(`.cc-scroll`, {
+  interval: 100,
+})
